52e24e0d686f9f95e82e165982a43f7657de7465
AkhilVinayakp <nirvanaosborn@gmail.com>
2020-06-15 17:14:37 +0530

    test scehma method

--- projbackend/models/test.js
+++ projbackend/models/test.js
@@ -1,3 +1,5 @@
+/*
+ testing on 15-06-2020 for schema modeling
 const mongoose = require("mongoose")
 let schema1 = new mongoose.Schema({name:String, lastname:String})
 
@@ -22,7 +24,7 @@ schema1.method={
 }
 schema1.method.test = function(){ return 1}
 
- */
+
 
 
 let model = mongoose.model("s1",schema1)
@@ -33,6 +35,43 @@ rm.fullname = 'akv vin'
 console.log(rm.name)
 console.log(rm.lastname)
 
+ */
+
+/* extended testing for schema methods and static methods
+ */
+
+const mongoose = require("mongoose")
+const testSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required: true
+    },
+    salt:String
+})
+
+/*
+testSchema.method("wishyou",function () {
+    return "good morning "+ this.name
+})
+
+ */
+
+testSchema.method={
+    test:function () {
+        return "hai"+this.name
+    }
+}
+
+
+// creating model
+let model = mongoose.model("nm",testSchema)
+let me = new model({name:"Akhil", password: "vinayak"})
+console.log(me.name)
+console.log(me.test)
 
 
 
