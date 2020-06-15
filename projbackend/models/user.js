7143ac4dcc69480b13a53434a2146fd921931d7d
AkhilVinayakp <nirvanaosborn@gmail.com>
2020-06-15 11:00:27 +0530

    usermodel has been exported

--- projbackend/models/user.js
+++ projbackend/models/user.js
@@ -1,1 +1,49 @@
-const mongoose = 
+const mongoose = require("mongoose")
+const schema = mongoose.Schema;
+
+let UserSchema = new schema({
+    name :{
+        type:String,
+        required:true,
+        maxlength:34,
+        trim:true
+    },
+    lastname:{
+        type: String,
+        maxlength: 32,
+        trim: true
+    },
+    email:{
+        type:String,
+        trim:true,
+        maxlength:55,
+        required: true,
+        unique:true
+    },
+    user_info:{
+        type:String,
+        maxlength:67,
+        trim:true
+    },
+
+
+    // need to be updated
+    password:{
+        type:String,
+        trim:true
+    },
+    salt : String,
+    role:{
+        type:Number,
+        default:0
+    },
+    purchases:{
+        type:Array,
+        default: []
+    }
+
+});
+
+// exporting the model using the schema created
+module.exports = mongoose.model("user",UserSchema)
+
