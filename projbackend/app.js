b0891089581a42e74c21bf642c630f82d5463635
AkhilVinayakp <nirvanaosborn@gmail.com>
2020-06-20 19:21:23 +0530

    mongodb connection ok

--- projbackend/app.js
+++ projbackend/app.js
@@ -9,6 +9,18 @@ const app = express();
 //assigning the port to run TODO change
 const port =8000;
 
+//creating the connection with mongodb
+mongoose.connect("mongodb://localhost:27017/test",{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useCreateIndex:true
+}).then(()=>{
+    console.log(`CONNECTION SUCCESS WITH MONGODB AT PORT 27017`);
+}).catch(reason => {
+    console.log(`error \n ${reason}`);
+});
+
+
 
 //listening to the port
 app.listen(port,()=>{
