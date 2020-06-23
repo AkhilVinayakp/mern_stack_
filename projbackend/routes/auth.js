d8667bd01559ca8622843c0a7770e736df573a9c
AkhilVinayakp <nirvanaosborn@gmail.com>
2020-06-23 20:18:28 +0530

    adding validation

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -2,8 +2,14 @@ const express = require("express");
 const router = express.Router();
 // adding the controllers to handle the routes
 const { sign_out, sign_up } = require("../controllers/auth");
+// applying validation
+const { check } = require("express-validator");
 
 router.get("/sign_out",sign_out);
-router.post("/sign_up",sign_up);
+router.post("/sign_up",[
+    check("name").isLength({min:3,max:32}).withMessage("the name should have minimum 3 and max 32 characters"),
+    check("password").isLength({min:3,max:20}).withMessage("the password length should be 3 to 20 ch"),
+    check("email").isEmail().withMessage("not a valid email")
+],sign_up);
 
 module.exports = router;
