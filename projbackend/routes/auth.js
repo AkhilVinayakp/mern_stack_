03aa40200d9d09543a0bf27712b56307e618778d
AkhilVinayakp <nirvanaosborn@gmail.com>
2020-06-23 12:59:59 +0530

    adding cortroller through external file

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
+// adding the controllers to handle the routes
+const { sign_out, sign_up } = require("../controllers/auth");
 
-router.get("/sign_out",(req,res)=>{
-    res.send("sign out successfully");
-});
+router.get("/sign_out",sign_out);
+router.post("/sign_up",sign_up);
 
 module.exports = router;
