462c629f245ff73e60bf62b284af1e85260a23b3
AkhilVinayakp <nirvanaosborn@gmail.com>
2020-06-23 12:58:59 +0530

    controller templates for sign_out and sign_up created

---
+++ projbackend/controllers/auth.js
@@ -0,0 +1,12 @@
+
+
+exports.sign_out = (req,res)=>{
+    res.json({
+        message:"sign out successfully"
+    })
+}
+exports.sign_up=(req,res)=>{
+    res.json({
+        message:"sign_up"
+    });
+};
