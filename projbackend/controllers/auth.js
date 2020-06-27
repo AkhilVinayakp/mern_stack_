80789ea87cbfdb83eb817f45a499b6d20be3862e
AkhilVinayakp <nirvanaosborn@gmail.com>
2020-06-27 12:44:50 +0530

    sign_in route handler

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,11 +1,15 @@
 const User = require("../models/user");
-const { validationResult } = require("express-validator");
+const { validationResult } = require("express-validator")
+// adding the token headers for authentication
+const jwt = require("jsonwebtoken");
+const expressJwt =require("express-jwt");
 
 exports.sign_out = (req,res)=>{
     res.json({
         message:"sign out successfully"
     })
 }
+//sign_up handler *************************************************************************************
 exports.sign_up=(req,res)=>{
     const errors = validationResult(req);
     if(!errors.isEmpty()){
@@ -26,3 +30,37 @@ exports.sign_up=(req,res)=>{
     })
 
 };
+
+//sign_in handler *************************************************************************************
+exports.sign_in=function (req,res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({
+            error: errors.array()[0].msg
+        })
+    }
+    //destructuring the request.body
+    const {email, password} = req.body;
+    User.findOne({email},(err,user)=>{
+        if(err)
+            return res.status(200).json({
+                error:"email does not exist"
+            })
+        if(!user.authenticate(password)){
+            return res.status(402).json({
+                error:"email password mismatch"
+            });
+            }
+        // creating jwt token
+        const token = jwt.sign({id:user._id},process.env.SECRET);
+        // storing inside cookies
+        res.cookie("token",token,{expire:new Date()+9999});
+        // sending the response back to the call
+        const {_id, name, email, role} = user;
+        res.status(200).json({
+            token:token,
+            user:{_id,name,email,role}
+        });
+    })
+
+}
