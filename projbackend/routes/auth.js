658cdedb311bea6035ffed718ccf68e255c99ea3
AkhilVinayakp <nirvanaosborn@gmail.com>
2020-06-28 12:42:25 +0530

    test route for authentication checking

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 // adding the controllers to handle the routes
-const { sign_out, sign_up, sign_in } = require("../controllers/auth");
+const { sign_out, sign_up, sign_in, secured } = require("../controllers/auth");
 // applying validation
 const { check } = require("express-validator");
 
@@ -17,5 +17,11 @@ router.post("/sign_in",[
     check("password").isLength({min:1}).withMessage("please enter your password password "),
     check("email").isEmail().withMessage("not a valid email")
 ],sign_in);
+
+
+// test route : secuiring routes
+router.get("/test", secured, (req,res)=>{
+	res.send("testing secured")
+})
 
 module.exports = router;
