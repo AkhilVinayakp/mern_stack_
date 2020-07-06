8d5d82793119a749af554cc5977c31f7fdf6ba5e
AkhilVinayakp <nirvanaosborn@gmail.com>
2020-07-06 17:18:12 +0530

    get rout isAuth fixed

--- projbackend/routes/user.js
+++ projbackend/routes/user.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const {get_UserById, getUser} = require("../controllers/user");
+const {isSigned, isAuthenticated,isAdmin } = require("../controllers/auth");
+ 
 
+router.param("userId",get_UserById);
 
-
+// routes for getting a user by id
+router.get("/user/:userId",[isSigned,isAuthenticated],getUser);
 
 
 module.exports = router
