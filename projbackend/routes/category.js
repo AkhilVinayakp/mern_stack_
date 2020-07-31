3723686e708752a9903018e6b718a5ad3af6ce62
AkhilVinayakp <nirvanaosborn@gmail.com>
2020-07-31 11:58:50 +0530

    adding route for creating new category

--- projbackend/routes/category.js
+++ projbackend/routes/category.js
@@ -2,11 +2,17 @@ const express = require('express')
 const router = express.Router()
 const {isSigned, isAuthenticated,isAdmin } = require("../controllers/auth");
 const {get_UserById} = require("../controllers/user");
-const {get_CatbyId} = require("../controllers/category");
+const {get_Category_byId, createCategory} = require("../controllers/category");
 
 // extracting the catagory from the parameter
-router.param('cat_id', get_CatbyId);
+router.param('cat_id', get_Category_byId);
+router.param("userId", get_UserById);
 
 
 
 // assigning  the different routes for the app
+// category/create/:userId for creating a new category for the tshirt * catagory name should be passed via json
+router.push("category/create/:userId",[isSigned,isAuthenticated,isAdmin], createCategory)
+
+// exporting the route
+module.exports = router;
